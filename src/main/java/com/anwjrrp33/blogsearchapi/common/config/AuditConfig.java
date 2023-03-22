8eb890944256fa1b9a407d1fa407db5e959b6842
anwjrrp33 <anwjrrp33@gmail.com>
2023-03-22 09:46:25 +0900

    feat: 공통 Entity 추가

---
+++ src/main/java/com/anwjrrp33/blogsearchapi/common/config/AuditConfig.java
@@ -0,0 +1,10 @@
+package com.anwjrrp33.blogsearchapi.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+
+}
---
+++ src/main/java/com/anwjrrp33/blogsearchapi/common/domain/BaseEntity.java
@@ -0,0 +1,20 @@
+package com.anwjrrp33.blogsearchapi.common.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @CreatedDate
+    private LocalDateTime createdDatetime;
+
+    @LastModifiedDate
+    private LocalDateTime updateDatetime;
+}
